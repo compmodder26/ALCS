7df632140f077da5230a484ddba7dd3134f7cb79
Brian Helm <bthelm2@gmail.com>
2017-02-23 16:57:19 -0500

    Incorporate the Adafruit_MCP3008 library for easy reading of the ADC

--- ALCS.py
+++ ALCS.py
@@ -21,6 +21,7 @@
 
 import RPi.GPIO as GPIO # Library that allows us to talk with the GPIO pins of the Raspberry Pi
 import time # Library that allows us to pause the program (in the case where we want to leave the light on for a certain amount of time or make it blink)
+import Adafruit_MCP3008 as ADC # Library that allows us to easily talk to the MCP3008 ADC (Analog to Digital Converter)
 
 # set up user friendly names for the GPIO pins we are working with
 LIGHT = 18 # Pin number we are going to control the light with
@@ -28,7 +29,7 @@ ADCCLOCK = 23 # Pin number connected to the ADC (Analog to Digital Converter) Cl
 ADCMOSI = 24 # Pin number connected to the ADC MOSI
 ADCMISO = 25 # Pin number connected to the ADC MISO
 ADCCS = 12 # Pin Number connected to the ADC CS
-ADCCHANNEL = 0 # Channel number that the pressure sensor is connected to of the ADC (0 - 7)
+ADCCHANNEL = 0 # Channel number of the ADC that the pressure sensor is connected to (0 - 7)
 
 # set up GPIO (General Purpose Input/Output)
 GPIO.setwarnings(False) # we don't want to see any warnings
@@ -36,12 +37,9 @@ GPIO.setmode(GPIO.BCM) # set how the numbers of the pins are interpreted (system
 
 # set up the pin modes
 GPIO.setup(LIGHT, GPIO.OUT) # we tell the raspberry pi that we want to use this pin to output signal to the light (sending a signal out)
-GPIO.setup(ADCCLOCK, GPIO.OUT) # we tell the raspberry pi that we want to use this pin to output signal to the Clock pin on the ADC (sending a signal out)
-GPIO.setup(ADCMOSI, GPIO.OUT) # we tell the raspberry pi that we want to use this pin to output signal to the MOSI pin on the ADC (sending a signal out)
-GPIO.setup(ADCMISO, GPIO.IN) # we tell the raspberry pi that we want to use this pin to receive information from the MISO pin on the ADC (receiving a signal)
-GPIO.setup(ADCCS, GPIO.OUT) # we tell the raspberry pi that we want to use this pin to output signal to the CS pin on the ADC (sending a signal out)
 
-
+# Set up ADC Reader
+adcReader = ADC.MCP3008(clk=ADCCLOCK, cs=ADCCS, miso=ADCMISO, mosi=ADCMOSI)
 
 # Function: cleanup()
 #
@@ -52,39 +50,11 @@ def cleanup():
 # Function: getPressureSensorValue()
 #
 # Will give the current value that the pressure sensor is reading.
-# Values range from 0 (nothing on the sensor) to 1023 (maximum weight the sensor can read)
+# Values range from 0 (nothing on the sensor) to 1023 (maximum pressure)
 def getPressureSensorValue():
-        GPIO.output(ADCCS, True)      # bring CS high
- 
-        GPIO.output(ADCCLOCK, False)  # start clock low
-        GPIO.output(ADCCS, False)     # bring CS low
- 
-        commandout = ADCCHANNEL
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(ADCMOSI, True)
-                else:
-                        GPIO.output(ADCMOSI, False)
-                commandout <<= 1
-                GPIO.output(ADCCLOCK, True)
-                GPIO.output(ADCCLOCK, False)
- 
-        pressureReading = 0
+	pressureValue = adcReader.read_adc(ADCCHANNEL)
 
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(ADCCLOCK, True)
-                GPIO.output(ADCCLOCK, False)
-                pressureReading <<= 1
-                if (GPIO.input(ADCMISO)):
-                        pressureReading |= 0x1
- 
-        GPIO.output(ADCCS, True)
-        
-        pressureReading >>= 1       # first bit is 'null' so drop it
-        return pressureReading
+	return pressureValue
 
 # Function: lightOn()
 #
@@ -100,12 +70,12 @@ def lightOn():
 def lightOff():
 	GPIO.output(LIGHT, False) # False means that we want to turn it off
 
-# Function: Blinky
+# Function: Blinky()
 #
 # Makes the light blink for a specified number of seconds
 #
-# Created by Natalie with the aid of Brian
-# Brian made the logic to make it blink for a specific amount of time
+# Created by Natalie with the help of Brian
+# Brian made the logic to make it run for a specific amount of time
 # Natalie included the code to turn the light on and off and to have it stay on
 # or off for a specific amount of time (everything within the while loop)
 def Blinky(secs):
@@ -122,7 +92,7 @@ try:
 #####################################################
 # Natalie's Main Program
 #####################################################
-	Blinky(10)
+	Blinky(5)
 #####################################################
 # END Natalie's Main Program
 #####################################################
